3eeba392599c25345bf0b7838c3c7c76e94b3285
TaikiMatsumoto <taiki.matsumoto.work@gmail.com>
2021-09-27 02:13:14 +0900

    route,view編集

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -78,6 +78,9 @@
             <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.dashboard')">
+                オーナー管理
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
